34a5a008145b156b0b006364d4b12920d068c741
Johnny King <31927132+JohnKing93@users.noreply.github.com>
2018-11-17 15:03:32 -0500

    Heroku updates

--- app/public/js/form.js
+++ app/public/js/form.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    
+
     // Handle form validaton
     var forms = document.getElementsByClassName('needs-validation');
     var validation = Array.prototype.filter.call(forms, function(form) {
